c53539ce85459e88ade5820a4f1a8dd5736f9152
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-10-21 03:11:20 +0300

    Update _strlen.c

--- _strlen.c
+++ _strlen.c
@@ -14,3 +14,45 @@ int _strlen(char *s)
 
 	return (len);
 }
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string 1
+ * @src: strint 2
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, k;
+
+	i = 0, j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+		j++;
+	for (k = 0; k <= j; k++, i++)
+		dest[i] = src[k];
+	return (dest);
+}
+
+
+/**
+ * _strcpy - main function to copy
+ *
+ * @dest: destination to copy
+ * @src: src
+ *
+ * Return: a character value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
